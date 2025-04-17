4e991a26ec04e51ec6848e9ad2cd522a7efae974
Soren <danilo-258@hotmail.com>
2025-04-17 09:59:30 -0300

    feat: enhance requisitarPagina function with additional logging for AJAX request states

--- class/scripts/xmlrequest.js
+++ class/scripts/xmlrequest.js
@@ -1,8 +1,17 @@
 function requisitarPagina(url){
   const ajax = new XMLHttpRequest();
-
+  //Requisição não iniciada, state = 0
+  console.log(ajax.readyState)
+  //conexão estabelecida com o servidor, state = 1
   ajax.open('GET', url)
+  console.log(ajax.readyState)
+
+  //de alguma logica que fique olhando para o progsso da req
+  ajax.onreadystatechange = () => {
+    console.log(ajax.readyState)
+  }
+
   ajax.send()
 
-  console.log(ajax)
+  // console.log(ajax)
 }
