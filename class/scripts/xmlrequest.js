8b0028a7d657cefb89ce399c2c32276c8951f313
Soren <danilo-258@hotmail.com>
2025-04-18 03:56:03 -0300

    feat: improve loading indicator handling in requisitarPagina function

--- class/scripts/xmlrequest.js
+++ class/scripts/xmlrequest.js
@@ -1,16 +1,30 @@
 function requisitarPagina(url){
+
+  if(!document.getElementById('loading')){
+  const imgLoading = document.createElement('img')
+  imgLoading.id = 'loading'
+  imgLoading.src = '/ajax/class/scripts/loading.gif'
+  imgLoading.className = 'rounded mx-auto d-block'
+
+  document.getElementById('conteudo').appendChild(imgLoading)
+  }
   const ajax = new XMLHttpRequest();
   //Requisição não iniciada, state = 0
-  console.log(ajax.readyState)
+  // console.log(ajax.readyState)
   //conexão estabelecida com o servidor, state = 1
   ajax.open('GET', url)
-  console.log(ajax.readyState)
+  // console.log(ajax.readyState)
 
   //de alguma logica que fique olhando para o progsso da req
   ajax.onreadystatechange = () => {
-    console.log(ajax.readyState)
-  }
+    if(ajax.readyState === 4 && ajax.status === 200){
+      // console.log(ajax.readyState)
+      // console.log(ajax.status)
+      // console.log(ajax.responseText)
 
+      document.getElementById('conteudo').innerHTML = ajax.responseText
+  }
+  }
   ajax.send()
 
   // console.log(ajax)
